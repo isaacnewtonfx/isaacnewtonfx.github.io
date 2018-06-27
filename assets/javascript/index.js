8a4907aaced58bd0ec9e867d64bdfef428d06e0e
Isaac Newton <isaacnewtonfx@gmail.com>
2018-06-27 15:21:38 +0000

    added service worker

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,35 @@
 $(document).ready(function () {
 
-	var currencyListHtml = "";
 
+// ==============Registering the Service worker=============
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js').then(function(reg) { 
+			console.log("Service Worker Registered"); 
+
+			if (reg.waiting) {
+		      console.log("sw is waiting");
+		      return;
+		    }
+
+		    if (reg.installing) {
+		      console.log("sw is installing");
+		      return;
+		    }
+
+		    if (reg.active) {
+		      console.log("sw is active");
+		      return;
+		    }
+
+
+		});
+	}
+// ====================End of Service worker====================
+
+
+
+
+	var currencyListHtml = "";
 	loadCurrenciesOnline();
 
 	// Function to load currency data online, 
---
+++ sw.js
@@ -0,0 +1,52 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open("appcache").then(function(cache) {
+      return cache.addAll([
+      	'/',
+        '/assets/css/bootstrap.css',
+        '/assets/css/site.css',
+        '/assets/banner.jpg',
+        '/assets/javascript/jquery.js',
+        '/assets/javascript/bootstrap.js',
+        '/assets/javascript/index.js',
+        'https://free.currencyconverterapi.com/api/v5/currencies',        
+      ]);
+    }).catch(function(e){
+    	console.log(e);
+    })
+  );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+    	
+    	if(!cachedResponse){
+
+    		//fetch onlineResponse
+    		return fetch(event.request).then(function(onlineResponse){
+
+    			clonedOnlineResponse = onlineResponse.clone();
+
+	    		//add onlineResponse to caches
+	    		caches.open("appcache").then(function(cache) {	
+			      cache.put(event.request.url, clonedOnlineResponse);
+			    });
+
+	    		console.log("using an online response for: " + event.request.url);
+    			return onlineResponse;
+    		}).catch(function(e){
+    			console.log(e);
+    		});
+    		
+    	}
+
+    	console.log("cache found for: " + event.request.url);
+      return cachedResponse;
+    })
+  );
+
+});
